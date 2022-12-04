8a143cf0b04d3af6b0683a8c3b0b6b65a4eb8dae
royalejandro09 <roymartinezv@gmail.com>
2022-12-03 22:02:20 -0500

    new changes

--- movies-api/target/generated-sources/annotations/com/digitalmedia/movies/mapper/MovieMapperImpl.java
+++ movies-api/target/generated-sources/annotations/com/digitalmedia/movies/mapper/MovieMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-03T20:51:03-0500",
+    date = "2022-12-03T22:00:39-0500",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
