3fa44beba07aa45e4479ce759011a6bcda743d2f
royalejandro09 <roymartinezv@gmail.com>
2022-12-03 22:10:23 -0500

    docker file to bills ms

--- ms-bills/src/test/java/com/msbills/MsBillsApplicationTests.java
+++ ms-bills/src/test/java/com/msbills/MsBillsApplicationTests.java
@@ -3,7 +3,7 @@ package com.msbills;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MsBillsApplicationTests.class)
 class MsBillsApplicationTests {
     @Test
     void contextLoads() {
