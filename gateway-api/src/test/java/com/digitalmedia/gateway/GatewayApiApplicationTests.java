c19473d87552c1331a25c66fde962cd6c02eec64
royalejandro09 <roymartinezv@gmail.com>
2022-12-03 22:32:43 -0500

    new changes ++

--- gateway-api/src/test/java/com/digitalmedia/gateway/GatewayApiApplicationTests.java
+++ gateway-api/src/test/java/com/digitalmedia/gateway/GatewayApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.digitalmedia.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatewayApiApplicationTests.class)
 class GatewayApiApplicationTests {
 
   @Test
